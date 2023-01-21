82de55632587195fcbe3feb6682af321fa6ed1bf
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2023-01-21 13:29:19 +0100

    Update database.js

--- handlers/database.js
+++ handlers/database.js
@@ -1,6 +1,7 @@
 const { MongoDBConnector } = require('discord.js-v14-helper');
 
 module.exports = (client, config) => {
-    new MongoDBConnector(require('../config/main').database.mongodb_uri)
-        .startConnecting();
+    const connector = new MongoDBConnector(require('../config/main').database.mongodb_uri);
+    
+    connector.start();
 };
