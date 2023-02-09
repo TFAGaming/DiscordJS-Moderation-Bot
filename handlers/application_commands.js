471ad8189491df518731401a0ff10079df145e17
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2023-02-09 15:43:08 +0100

    Update application_commands.js

--- handlers/application_commands.js
+++ handlers/application_commands.js
@@ -1,61 +1,87 @@
-const fs = require('fs');
-const { PermissionsBitField } = require('discord.js');
-const { betterConsole, Colors, ApplicationCommandsRegister } = require('discord.js-v14-helper');
+const client = require('../index');
+const config = require('../config/main');
+const { ModulesSchema } = require('../schemas/main');
 
-module.exports = (client, config) => {
-    let commands = [];
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.isChatInputCommand() || interaction.isUserContextMenuCommand() || interaction.isMessageContextMenuCommand()) {
+        const command = await client.commands.get(interaction.commandName);
 
-    fs.readdirSync('./commands/').forEach((dir) => {
-        const files = fs.readdirSync('./commands/' + dir)
-            .filter((file) => file.endsWith('.js'))
+        if (!command) return interaction.reply({
+            content: `\`❌\` Invalid command, please try again later.`,
+            ephemeral: true
+        });
 
-        for (let file of files) {
-            let pulled = require('../commands/' + dir + '/' + file);
+        try {
+            const data = await ModulesSchema.findOne({
+                guild: interaction.guild.id
+            });
 
-            if (pulled.name && pulled.type) {
-                console.log(
-                    betterConsole('Loaded application command: ' + file + '.', {
-                        color: Colors.GREEN
-                    })
-                );
-
-                if (pulled.description) {
-                    commands.push(
-                        {
-                            name: pulled.name,
-                            description: pulled.description,
-                            type: 1,
-                            options: pulled.options ? pulled.options : null,
-                            default_permission: null, // No need because it's deprecated.
-                            default_member_permissions: null, // "role_perms" replaced this.
-                            nsfw: false // Because there is no NSFW commands.
-                        }
-                    );
-                } else {
-                    commands.push(
-                        {
-                            name: pulled.name,
-                            type: pulled.type
-                        }
-                    );
+            if (data) {
+                if (command.category) {
+                    if (data.modules.includes(command.category)) {
+                        return interaction.reply({
+                            content: `\`❌\` This command is currently disabled in the module **${command.category}**.`,
+                            ephemeral: true
+                        });
+                    };
                 };
-
-                client.commands.set(pulled.name, pulled);
-            } else {
-                console.log(
-                    betterConsole('[!] Received empty property \'name\' or \'type\' in ' + file + '.', {
-                        color: Colors.RED
-                    })
-                );
-
-                continue;
             };
-        };
-    });
 
-    const register = new ApplicationCommandsRegister(config.client.token, config.client.id)
-        .setApplicationCommands(commands)
-        .setRestVersion(10);
-    
-    register.start();
-};
+            if (command.owner_only && command.owner_only === true) {
+                if (config.users.owner !== interaction.user.id) {
+                    return interaction.reply({
+                        content: `\`❌\` Sorry but this command is restricted for the bot owner only!`,
+                        ephemeral: true
+                    });
+                };
+            };
+
+            if (command.developers_only && command.developers_only === true) {
+                if (config.users?.developers && config.users?.developers?.length > 0) {
+                    if (!config.users.developers.some((dev) => interaction.user.id === dev)) return interaction.reply({
+                        content: `\`❌\` Sorry but this command is restricted for developers only!`,
+                        ephemeral: true
+                    });
+                };
+            };
+
+            if (command.role_perms && command.role_perms !== null) {
+                if (Array.isArray(command.role_perms)) {
+                    if (command.role_perms?.length > 0) {
+                        let boolean = false;
+
+                        await command.role_perms.forEach((r) => {
+                            const role = interaction.guild.roles.cache.get(r);
+
+                            if (!role) return;
+
+                            if (!interaction.member.roles) boolean = false;
+                            if (interaction.member.roles.cache.some((r1) => r1.id === role.id)) boolean = true;
+                        });
+
+                        if (boolean === false) return interaction.reply({
+                            content: `\`❌\` Sorry but you are not allowed to use this command!`,
+                            ephemeral: true
+                        });
+                    };
+                } else if (typeof command.role_perms === 'string') {
+                    const role = interaction.guild.roles.cache.get(command.role_perms);
+
+                    if (role) {
+                        if (!interaction.member.roles.cache.has(role)) return interaction.reply({
+                            content: `\`❌\` Sorry but you are not allowed to use this command!`,
+                            ephemeral: true
+                        });
+                    };
+                };
+            };
+
+            command.run(client, interaction, config);
+        } catch (err) {
+            console.error(`[!] Failed to run the command \'${interaction.commandName}\'.`);
+            console.log(err);
+        } finally {
+            console.log(`[i] ${interaction.user.username} has used the command \'${interaction.commandName}\'.`);
+        };
+    } else return;
+});
