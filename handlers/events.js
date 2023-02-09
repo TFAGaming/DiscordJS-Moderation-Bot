d9bfaa4e78f288d3094310ebc8bc5809e220edc4
T.F.A <92172698+TFAGaming@users.noreply.github.com>
2023-02-09 15:43:24 +0100

    Update events.js

--- handlers/events.js
+++ handlers/events.js
@@ -1,14 +1,12 @@
 const fs = require('fs');
-const { betterConsole, Colors } = require('discord.js-v14-helper');
+const { BetterConsoleLogger, Colors } = require('discord.js-v14-helper');
 
 module.exports = (client, config) => {
     for (let file of fs.readdirSync('./events')) {
         require('../events/' + file);
         
-        console.log(
-            betterConsole(`Loaded client event: ${file}`, {
-                color: Colors.GREEN
-            })
-        );
+        new BetterConsoleLogger('Loaded client event: ' + file + '.')
+            .setTextColor(Colors.Green)
+            .log(true);
     };
 };
